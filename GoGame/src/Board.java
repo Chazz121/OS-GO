ab06e1f4ca75a8ef7fd1e13cf051376b2629b46a
Chazz121 <chrlsmitch123@gmail.com>
2018-11-19 12:47:00 -0500

    added board

--- GoGame/src/Board.java
+++ GoGame/src/Board.java
@@ -1,14 +1,21 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class Board {
-	public Board() {
+	private BufferedImage blankBoard;
+
+	public Board()  {
 		JFrame frame = new JFrame("Go Game");
 		JPanel jp = new JPanel();
 		Dimension d = new Dimension(1280,720);
@@ -18,10 +25,17 @@ public class Board {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		
+		try {
+			this.blankBoard = ImageIO.read(new File("src/BlankGoBoard.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		JLabel lab = new JLabel("hi");
-		frame.getContentPane().add(jp, BorderLayout.CENTER);
-
+		JLabel boardPic = new JLabel(new ImageIcon(blankBoard));
+		boardPic.setSize(d);
+		frame.getContentPane().add(boardPic, BorderLayout.CENTER);
+		
 		
 		frame.setSize(d);
 
